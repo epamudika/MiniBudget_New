fc24b5a2d34910eb5d751cc0b9528b3c65c21eac
epamudika <erandi201326@gmail.com>
2026-04-26 10:52:40 +0530

    Initial Commit

---
+++ MiniBudget_New/MiniBudget_NewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniBudget_NewApp.swift
+//  MiniBudget_New
+//
+//  Created by Erandi Pathirana on 2026-04-26.
+//
+
+import SwiftUI
+
+@main
+struct MiniBudget_NewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
